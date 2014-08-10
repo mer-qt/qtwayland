1fd482ba1ef11681d8df2acb0f4b9e07294ad48e
Jan Arne Petersen <jan.petersen@kdab.com>
2014-08-10 06:54:55 +0200

    Add support for setMask to set input region
    Also fix input region handling in compositor.
    
    Change-Id: If88ad6a03443526eddee045d7af54daf5057373a
    Done-with: Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
    Reviewed-by: Giulio Camuffo <giulio.camuffo@jollamobile.com>

--- src/client/qwaylandwindow_p.h
+++ src/client/qwaylandwindow_p.h
@@ -140,6 +140,8 @@ public:
     void raise() Q_DECL_OVERRIDE;
     void lower() Q_DECL_OVERRIDE;
 
+    void setMask(const QRegion &region) Q_DECL_OVERRIDE;
+
     void requestActivateWindow() Q_DECL_OVERRIDE;
     bool isExposed() const Q_DECL_OVERRIDE;
     void unfocus();
@@ -226,6 +228,7 @@ protected:
     int mMouseSerial;
 
     Qt::WindowState mState;
+    QRegion mMask;
 
     QWaylandShmBackingStore *mBackingStore;
 
